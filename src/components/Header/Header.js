23b7ab1cf7180e103c50afadb261430987c48da1
leon0113 <tahjib.leon399@gmail.com>
2023-02-20 23:13:46 +0600

    nav added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Welcome to Tshirt shop</h2>
+            <nav className='nav'>
+                <Link to="/home">Home</Link>
+                <Link to="/or">Order Review</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
