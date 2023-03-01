7b0d57565547933dd8f6911312238e0b212c575c
leon0113 <tahjib.leon399@gmail.com>
2023-03-02 01:03:39 +0600

    p tag

--- src/components/Uncle/Uncle.js
+++ src/components/Uncle/Uncle.js
@@ -4,6 +4,9 @@ const Uncle = ({ house }) => {
     return (
         <div>
             <h2>This is Uncle component</h2>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
             <p>House : {house}</p>
         </div>
     );
